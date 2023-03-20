ae6274106bd6a2f75cb51a487369c750a585fe99
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2023-03-20 23:53:05 +0530

    2nd commit

--- Reactjs/reactjs/src/App.js
+++ Reactjs/reactjs/src/App.js
@@ -1,39 +1,39 @@
 
-// import Navbar from './Components/Navbar';
-// import logo from './images/logo.png';
-// // import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import logo from './images/logo.png';
+// import Login from './Components/Login';
 
-
-// function App() {
-//   return (
-//     <div className='App'>
-    
-//        <img src={logo} alt=""></img><Navbar />
-       
-       
-//     </div>
-//   )
-// }
-
-// export default App;
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/">
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    <div className='App'>
+    
+       <img src={logo} alt=""></img><Navbar />
+       
+       
+    </div>
+  )
 }
 
-export default App;
+// export default App;
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Login from './Components/Login';
+// import Signup from './Components/Signup';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/signup">
+//           <Signup />
+//         </Route>
+//         <Route path="/">
+//           <Login />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
